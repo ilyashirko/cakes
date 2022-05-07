ad7f6289b577367876ba3ec13602aeb6bc80d288
Vitaly Borodulin <rainbowf@mail.ru>
2022-05-07 14:33:55 +0500

    delete defaults

--- cakes/settings.py
+++ cakes/settings.py
@@ -146,5 +146,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = env.str('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = env.int('EMAIL_PORT', 587)
 EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', True)
-EMAIL_HOST_USER = env.str('EMAIL_HOST_USER', 'example@gmail.ru')
-EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD', 'email_password')
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
