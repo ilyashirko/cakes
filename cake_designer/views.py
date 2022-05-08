aeff2e6cc22496a38dee0c244fdf5a4644516d04
ilyashirko <82837921+ilyashirko@users.noreply.github.com>
2022-05-08 12:01:28 +0300

    add yookassa link from main

--- cake_designer/views.py
+++ cake_designer/views.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .models import (
     Berry,
@@ -80,7 +80,10 @@ def index(request):
             cost=cost,
             utm=utm
         )
+        from .helpers import create_payment
+        payment = create_payment(cost)
 
+        return redirect(payment['url'])
     context = {}
     return render(request, 'index.html', context)
 
