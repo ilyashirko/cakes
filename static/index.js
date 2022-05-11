b59b3b1f4c4a3ba955ac70136d8f37cb0082b217
ilyashirko <82837921+ilyashirko@users.noreply.github.com>
2022-05-11 19:53:32 +0300

    null promo accepted

--- static/index.js
+++ static/index.js
@@ -80,12 +80,6 @@ Vue.createApp({
                     }
                     return ' почту';
                 },
-                promo: (value) => {
-                    if (value) {
-                        return true;
-                    }
-                    return ' промокод';
-                },
                 address: (value) => {
                     if (value) {
                         return true;
@@ -152,7 +146,7 @@ Vue.createApp({
             let W = this.Words ? this.Costs.Words : 0
             let P1 = this.Promo == 'FIRST' ? this.Costs.Promo[2] : 0
             let P2 = this.Promo == 'WYLSA' ? this.Costs.Promo[1] : 0
-            let P3 = this.Promo == '' ? this.Costs.Promo[1] : 0
+            let P3 = this.Promo == null ? this.Costs.Promo[0] : 0
             return this.Costs.Levels[this.Levels] + this.Costs.Forms[this.Form] +
                 this.Costs.Toppings[this.Topping] + this.Costs.Berries[this.Berries] +
                 this.Costs.Decors[this.Decor] + W - P1 - P2
