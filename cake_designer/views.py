d4ea8c5928b25cb2024be1d5b6a09d1202d3b92e
ilyashirko <82837921+ilyashirko@users.noreply.github.com>
2022-05-11 12:13:03 +0300

    save order during account entry

--- cake_designer/views.py
+++ cake_designer/views.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+from urllib.parse import urljoin
 
+from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import redirect, render
@@ -18,6 +20,79 @@ from .models import (
     Utm
 )
 
+def save_and_pay_order(request):
+    if 'utm_source' in request.session:
+            utm, _ = Utm.objects.get_or_create(
+                source=request.session["utm_source"],
+                medium=request.session["utm_medium"],
+                campaign=request.session["utm_campaign"],
+                content=request.session["utm_content"],
+                term=request.session["utm_term"]
+            )
+    else:
+        utm = None
+
+    delivery, _ = Delivery.objects.get_or_create(
+        address=request.GET["ADDRESS"],
+        datetime=datetime.strptime(
+            f'{request.GET["DATE"]}{request.GET["TIME"]}',
+            "%Y-%m-%d%H:%M"
+        ),
+        comment=request.GET.get("DELIVCOMMENTS")
+    )
+
+    customer, _ = Customer.objects.get_or_create(
+        user=request.user
+    )
+    customer.mailbox = request.GET['EMAIL']
+    customer.first_name = request.GET['NAME']
+    customer.phonenumber = request.GET['PHONE']
+    customer.save()
+
+    levels = Level.objects.get(num=request.GET["LEVELS"])
+    form = Form.objects.get(num=request.GET["FORM"])
+    topping = Topping.objects.get(num=request.GET["TOPPING"])
+
+    cost = levels.cost + form.cost + topping.cost
+
+    berries, decoration = None, None
+    if request.GET.get("BERRIES"):
+        berries = Berry.objects.get(num=request.GET["BERRIES"])
+        cost += berries.cost
+    if request.GET.get("DECOR"):
+        decoration = Decoration.objects.get(num=request.GET["DECOR"])
+        cost += decoration.cost
+
+    try:
+        promo = Promo.objects.get(title=request.GET.get("PROMO"))
+    except Promo.DoesNotExist:
+        promo = None
+    if promo and promo.is_valid:
+        promo_cost = cost - promo.number
+    else:
+        promo_cost = cost
+    payment = create_payment(
+        promo_cost,
+        request.build_absolute_uri('/accounts/profile/')
+    )
+    order = Order.objects.create(
+        levels=levels,
+        form=form,
+        topping=topping,
+        berries=berries,
+        decoration=decoration,
+        signature=request.GET.get("WORDS"),
+        comment=request.GET.get("COMMENTS"),
+        customer=customer,
+        delivery=delivery,
+        cost=cost,
+        promo=promo,
+        promo_cost=promo_cost,
+        utm=utm,
+        payment_id=payment['id']
+    )
+    return payment['url']
+
 
 def index(request):
     if 'utm_source' in request.GET:
@@ -28,78 +103,10 @@ def index(request):
         request.session["utm_term"] = request.GET.get("utm_term")
 
     if 'LEVELS' in request.GET and request.user.is_authenticated:
-        if 'utm_source' in request.session:
-            utm, _ = Utm.objects.get_or_create(
-                source=request.session["utm_source"],
-                medium=request.session["utm_medium"],
-                campaign=request.session["utm_campaign"],
-                content=request.session["utm_content"],
-                term=request.session["utm_term"]
-            )
-        else:
-            utm = None
-
-        delivery, _ = Delivery.objects.get_or_create(
-            address=request.GET["ADDRESS"],
-            datetime=datetime.strptime(
-                f'{request.GET["DATE"]}{request.GET["TIME"]}',
-                "%Y-%m-%d%H:%M"
-            ),
-            comment=request.GET.get("DELIVCOMMENTS")
-        )
-
-        customer, _ = Customer.objects.get_or_create(
-            user=request.user
-        )
-        customer.mailbox = request.GET['EMAIL']
-        customer.first_name = request.GET['NAME']
-        customer.phonenumber = request.GET['PHONE']
-        customer.save()
-
-        levels = Level.objects.get(num=request.GET["LEVELS"])
-        form = Form.objects.get(num=request.GET["FORM"])
-        topping = Topping.objects.get(num=request.GET["TOPPING"])
-
-        cost = levels.cost + form.cost + topping.cost
-
-        berries, decoration = None, None
-        if request.GET.get("BERRIES"):
-            berries = Berry.objects.get(num=request.GET["BERRIES"])
-            cost += berries.cost
-        if request.GET.get("DECOR"):
-            decoration = Decoration.objects.get(num=request.GET["DECOR"])
-            cost += decoration.cost
-
-        try:
-            promo = Promo.objects.get(title=request.GET.get("PROMO"))
-        except Promo.DoesNotExist:
-            promo = None
-        if promo and promo.is_valid:
-            promo_cost = cost - promo.number
-        else:
-            promo_cost = cost
-        payment = create_payment(
-            promo_cost,
-            request.build_absolute_uri('accounts/profile/')
-        )
-        Order.objects.create(
-            levels=levels,
-            form=form,
-            topping=topping,
-            berries=berries,
-            decoration=decoration,
-            signature=request.GET.get("WORDS"),
-            comment=request.GET.get("COMMENTS"),
-            customer=customer,
-            delivery=delivery,
-            cost=cost,
-            promo=promo,
-            promo_cost=promo_cost,
-            utm=utm,
-            payment_id=payment['id']
-        )
-        return redirect(payment['url'])
+        payment_url = save_and_pay_order(request)
+        return redirect(payment_url)
     if 'LEVELS' in request.GET and not request.user.is_authenticated:
+        request.session['order'] = request.GET
         return redirect('login')
         
     context = {}
@@ -108,6 +115,12 @@ def index(request):
 
 @login_required(login_url='login')
 def lk(request):
+    if 'order' in request.session:
+        request.GET = request.session['order']
+        del request.session['order']
+        payment_url = save_and_pay_order(request)
+        return redirect(payment_url)
+
     if 'EMAIL' in request.GET:
         customer, _ = Customer.objects.get_or_create(
             user=request.user
